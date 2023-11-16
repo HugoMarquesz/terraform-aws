a0ee110720b52ad933fde5a89044cf4b01aee835
HugoMarquesz <eldersovisk97@outlook.com>
2023-11-16 18:07:13 -0300

    primeiro commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1" # 
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0230bd60aa48260c6" 
+  instance_type = "t2.micro"              
+
+  tags = {
+    Name = "MinhaInstanciaEC2"
+  }
+}
